378a088df8c7b6921a5ca72abde9a44503aded1c
Dmaledicte <matiasjesusmartel@outlook.es>
2023-02-24 19:59:50 -0300

    Incorporacion de favicon, retoque de tildes y font

--- fut-calculator.js
+++ fut-calculator.js
@@ -7,16 +7,16 @@ const pDerrotas = document.querySelector(`#puntosRestantesDerrotas`);
 const recompensas = document.querySelector(`#recompensas`);
 const resultImage = document.querySelector('#result-image')
 const tresholds = [
-    {name: 'Rango 10',puntos: 4,recompensa: '500 puntos FC, 1 Sobre De Mezcla De Jugadores Únicos y 1 Elección de jugador FUT Champions (intransferible)',},
-    {name: 'Rango 9',puntos: 12,recompensa: '500 puntos FC, 5.000 monedas FUT, 2 sobres Oro Jumbo Premium, 1 Sobre De Jugadores Top De Oro y 1 Elección de jugador FUT Champions (intransferible)',},
-    {name: 'Rango 8',puntos: 24,recompensa: '750 puntos FC, 10.000 monedas FUT, 2 sobres Oro Jumbo Premium, 1 Megasobre, 1 Sobre De Jugadores Top De Oro y 2 Elección de jugador FUT Champions (intransferible)',},
-    {name: 'Rango 7',puntos: 36,recompensa: '750 puntos FC, 15.000 monedas FUT, 1 Sobre De Jugadores Únicos, 1 Sobre Jumbo De Jugadores Únicos, 2 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 6',puntos: 45,recompensa: '1.000 puntos FC, 25.000 monedas FUT, 1 Sobre TOTW Premium, 1 Sobre De Jugadores Únicos, 1 Sobre Ultimate, 2 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 5',puntos: 51,recompensa: '1.250 puntos FC, 30.000 monedas FUT, 1 Sobre TOTW Premium, 1 Sobre De Jugadores Únicos, 1 Sobre Ultimate, 3 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 4',puntos: 60,recompensa: '1.250 puntos FC, 50.000 monedas FUT, 1 Sobre TOTW Premium, 1 Sobre Jumbo De Jugadores Únicos, 1 Sobre Ultimate, 3 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 3',puntos: 67,recompensa: '1.250 puntos FC, 75.000 monedas FUT, 2 Sobres TOTW Premium, 1 Sobre Ultimate, 3 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 2',puntos: 72,recompensa: '1.250 puntos FC, 100.000 monedas FUT, 2 Sobres TOTW Premium, 2 Sobres Ultimate, 3 elecciones de jugadores FUT Champions (intransferibles)',},
-    {name: 'Rango 1',puntos: 76,recompensa: '1.250 puntos FC, 100.000 monedas FUT, 3 Sobres TOTW Premium, 1 Sobre De Jugadores Únicos, 2 Sobres Ultimate, 3 elecciones de jugadores FUT Champions (intransferibles)',}
+    {name: 'Rango 10',puntos: 4,recompensa: '500 puntos FC / 1 Sobre De Mezcla De Jugadores unicos y 1 Eleccion de jugador FUT Champions (intransferible)',},
+    {name: 'Rango 9',puntos: 12,recompensa: '500 puntos FC / 5.000 monedas FUT / 2 sobres Oro Jumbo Premium / 1 Sobre De Jugadores Top De Oro y 1 Eleccion de jugador FUT Champions (intransferible)',},
+    {name: 'Rango 8',puntos: 24,recompensa: '750 puntos FC / 10.000 monedas FUT / 2 sobres Oro Jumbo Premium / 1 Megasobre / 1 Sobre De Jugadores Top De Oro y 2 Eleccion de jugador FUT Champions (intransferible)',},
+    {name: 'Rango 7',puntos: 36,recompensa: '750 puntos FC / 15.000 monedas FUT / 1 Sobre De Jugadores unicos / 1 Sobre Jumbo De Jugadores unicos / 2 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 6',puntos: 45,recompensa: '1.000 puntos FC / 25.000 monedas FUT / 1 Sobre TOTW Premium / 1 Sobre De Jugadores unicos / 1 Sobre Ultimate / 2 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 5',puntos: 51,recompensa: '1.250 puntos FC / 30.000 monedas FUT / 1 Sobre TOTW Premium / 1 Sobre De Jugadores unicos / 1 Sobre Ultimate / 3 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 4',puntos: 60,recompensa: '1.250 puntos FC / 50.000 monedas FUT / 1 Sobre TOTW Premium / 1 Sobre Jumbo De Jugadores unicos / 1 Sobre Ultimate / 3 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 3',puntos: 67,recompensa: '1.250 puntos FC / 75.000 monedas FUT / 2 Sobres TOTW Premium / 1 Sobre Ultimate / 3 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 2',puntos: 72,recompensa: '1.250 puntos FC / 100.000 monedas FUT / 2 Sobres TOTW Premium / 2 Sobres Ultimate / 3 elecciones de jugadores FUT Champions (intransferibles)',},
+    {name: 'Rango 1',puntos: 76,recompensa: '1.250 puntos FC / 100.000 monedas FUT / 3 Sobres TOTW Premium / 1 Sobre De Jugadores unicos / 2 Sobres Ultimate / 3 elecciones de jugadores FUT Champions (intransferibles)',}
 ]
 let partidosTotales = 20
 let puntosTotales = 100
@@ -154,11 +154,11 @@ function calcularPuntos () {
                 victoriasNecesarias = 0;
             }
             if (victoriasNecesarias > puntosDerrotas) {
-                pDerrotas.innerText = `Estas en ${tierActual}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
+                pDerrotas.innerText = `Estas en ${tierActual}. Ya no llegas al siguiente tier... EA se te esta cagando de shisa`;
                 recompensas.innerText = `Tu recompensa es: ${recompensa}`; 
                 resultImage.setAttribute('src', './img/barbakahn.png');     
             } else {
-            pDerrotas.innerText = `Perdiendo lo que queda estas en ${tierActual}, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
+            pDerrotas.innerText = `Perdiendo lo que queda estas en ${tierActual}... necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
             recompensas.innerText = `Tu recompensa es: ${recompensa}`; 
             if (tresholds[i - 1].puntos >= 45) {
             resultImage.setAttribute('src', './img/result.png')}
