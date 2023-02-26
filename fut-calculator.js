2348468a8a3307cd29a8eecb12e4a3a6e08bac47
Dmaledicte <matiasjesusmartel@outlook.es>
2023-02-25 21:36:47 -0300

    Implementacion de racha de victorias y derrotas, forma y retoques visuales en css a los inputs, cambios en la paleta de colores

--- fut-calculator.js
+++ fut-calculator.js
@@ -3,6 +3,7 @@ const inputObtenidos = document.querySelector(`#puntosObtenidos`);
 const inputRestantes = document.querySelector(`#partidosRestantes`);
 const btn = document.querySelector(`#calcular`);
 const pHipoteticos = document.querySelector(`#puntosTotalesHipoteticos`);
+const rachaDePartidos = document.querySelector(`#racha`)
 const pDerrotas = document.querySelector(`#puntosRestantesDerrotas`);
 const recompensas = document.querySelector(`#recompensas`);
 const resultImage = document.querySelector('#result-image')
@@ -33,125 +34,35 @@ function nextTier(tierSiguiente) {
 
 btn.addEventListener(`click`, calcularPuntos);
 
-// function calcularPuntos () {
-//     if (inputJugados.value > partidosTotales || inputObtenidos.value > puntosTotales) {
-//         alert ("Revisa los inputs");
-//         return
-//     }
-//     let partidosRestantes = partidosTotales - Number(inputJugados.value);
-//     const puntosObtenidos = Number(inputObtenidos.value);
-//     const puntosDerrotas = partidosRestantes
-//     const puntosRestantesPotenciales = (puntosDerrotas * 4) + puntosObtenidos;
-//     const puntosSumados = puntosObtenidos + puntosDerrotas;
-//     const tresholds = [24,36,45,51,60,67,72,76]
-//     if (puntosSumados >= tresholds[0] && puntosSumados < tresholds[1]) {
-//             const victoriasNecesarias = nextTier(tresholds[1]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en ${tresholds[0]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 24, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/badresult.png')};
-//     } 
-//     else if (puntosSumados >= tresholds[1] && puntosSumados < tresholds[2]) {
-//             const victoriasNecesarias = nextTier(tresholds[2]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//             pDerrotas.innerText = `Estas en ${tresholds[1]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//             resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {
-//             pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 36, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/badresult.png')};
-//     }
-//     else if (puntosSumados >= tresholds[2] && puntosSumados < tresholds[3]) {
-//             const victoriasNecesarias = nextTier(tresholds[3]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en ${tresholds[2]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 45, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/badresult.png')};
-//     }
-//     else if (puntosSumados >= tresholds[3] && puntosSumados < tresholds[4]) {
-//             const victoriasNecesarias = nextTier(tresholds[4]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en ${tresholds[3]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 51, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/result.png')};
-//     }
-//     else if (puntosSumados >= tresholds[4] && puntosSumados < tresholds[5]) {
-//             const victoriasNecesarias = nextTier(tresholds[6]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en ${tresholds[4]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 60, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/result.png')};
-//     }
-//     else if (puntosSumados >= tresholds[5] && puntosSumados < tresholds[6]) {
-//             const victoriasNecesarias = nextTier(tresholds[7]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en ${tresholds[5]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 67, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/result.png')};
-//     }
-//     else if (puntosSumados >= tresholds[6] && puntosSumados < tresholds[7]) {
-//         if (victoriasNecesarias > puntosDerrotas) {
-//             pDerrotas.innerText = `Estas en ${tresholds[6]}. Ya no llegas al siguiente tier, EA se te esta cagando de shisa`;
-//             resultImage.setAttribute('src', './img/barbakahn.png');     
-//         } else {pDerrotas.innerText = `Perdiendo lo que queda llegas al tier de 72, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//             resultImage.setAttribute('src', './img/result.png')};
-//     }
-//     else if (puntosSumados >= tresholds[7]) {
-//             pDerrotas.innerText = `Superas el tier de 76`
-//             resultImage.setAttribute('src', './img/result.png');
-//     } else {
-//             const victoriasNecesarias = nextTier(tresholds[0]);
-//             if (victoriasNecesarias > puntosDerrotas) {
-//                 pDerrotas.innerText = `Estas en el culo de la liga, ya no llegas ni a tramontina oxidado 4`;
-//                 resultImage.setAttribute('src', './img/barbakahn.png');     
-//             } else {
-//                 pDerrotas.innerText = `Estas en el culo de la liga, necesitas ${victoriasNecesarias} victorias para alcanzar el siguiente tier`;
-//                 resultImage.setAttribute('src', './img/badresult.png');
-//             }
-//     }
-//     pHipoteticos.innerText = `Ganando todo llegarias a ${puntosRestantesPotenciales}`
-// }
-
-// Supongamos que tienes un array de objetos llamado "objetos" y una variable llamada "puntosSumados"
-// let objetos = [
-//     { id: 1, puntos: 10 },
-//     { id: 2, puntos: 20 },
-//     { id: 3, puntos: 15 }
-//   ];
-//   let puntosSumados = 12;
-  
-//   // Recorre el array de objetos y compara el valor de "puntos" de cada objeto con "puntosSumados"
-//   for (let i = 0; i < objetos.length; i++) {
-//     if (objetos[i].puntos > puntosSumados) {
-//       // Si el valor de "puntos" del objeto es mayor que "puntosSumados", ese es el objeto que estás buscando
-//       console.log("El objeto con el ID " + objetos[i].id + " tiene más puntos que " + puntosSumados);
-//       break; // Puedes detener el bucle una vez que encuentres el objeto que buscas
-//     }
-//   }
-
 function calcularPuntos () {
-    if (inputJugados.value > partidosTotales || inputObtenidos.value > puntosTotales) {
+    if (inputJugados.value > partidosTotales || inputObtenidos.value > puntosTotales || inputJugados.value == "" || inputObtenidos == "") {
         alert ("Revisa los inputs");
         return
     }
     let partidosRestantes = partidosTotales - Number(inputJugados.value);
+    const partidosJugados = Number(inputJugados.value);
     const puntosObtenidos = Number(inputObtenidos.value);
     const puntosDerrotas = partidosRestantes
     const puntosRestantesPotenciales = (puntosDerrotas * 4) + puntosObtenidos;
     const puntosSumados = puntosObtenidos + puntosDerrotas;
     
+    const victoriasDigitadas = Math.round((puntosObtenidos - 1 * partidosJugados) / 3);
+    const derrotasDigitadas = partidosJugados - victoriasDigitadas;
+    const formaActual = victoriasDigitadas - derrotasDigitadas
+    let forma = formaActual;
+
+    if (forma < -15) {forma = -15}
+    else if (forma > 15) {forma = 15}
+    
+    if(formaActual > 0) {
+        forma = "+" + forma
+    };
+
     for (let i = 0; i < tresholds.length; i++) {
         if (tresholds[i].puntos > puntosSumados) {
             let victoriasNecesarias = nextTier(tresholds[i].puntos)
             const tierActual = tresholds[i - 1].name
             const recompensa = tresholds[i - 1].recompensa
-            // if (victoriasNecesarias === 1) {
-            //     victoriasNecesarias = 0;
-            // }
             if (victoriasNecesarias > puntosDerrotas) {
                 pDerrotas.innerText = `Estas en ${tierActual}. Ya no llegas al siguiente tier... EA se te esta cagando de shisa`;
                 recompensas.innerText = `Tu recompensa es: ${recompensa}`; 
@@ -177,4 +88,5 @@ function calcularPuntos () {
         } 
       }
     pHipoteticos.innerText = `Ganando todo llegarias a ${puntosRestantesPotenciales}`
+    rachaDePartidos.innerText = `Tu racha actual es de ${victoriasDigitadas}V y ${derrotasDigitadas}D. Tu forma es ${forma}`
 }
